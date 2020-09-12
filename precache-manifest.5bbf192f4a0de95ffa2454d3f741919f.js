b6e55f31e697c212a35d0854f66c565eaa655e93
Efim1298 <efimchuk12@gmail.com>
2020-09-12 03:19:28 +0300

    Updates

---
+++ precache-manifest.5bbf192f4a0de95ffa2454d3f741919f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e2bae4d9b5979186c9226342eb3aa3",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "58a8e0179b6446ee2bf5",
+    "url": "/weather/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "211ed3ba26fb2a04dbdb",
+    "url": "/weather/static/css/main.e160ac88.chunk.css"
+  },
+  {
+    "revision": "58a8e0179b6446ee2bf5",
+    "url": "/weather/static/js/2.63b9a7fc.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/weather/static/js/2.63b9a7fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211ed3ba26fb2a04dbdb",
+    "url": "/weather/static/js/main.b2889411.chunk.js"
+  },
+  {
+    "revision": "59ddb3fb53b0482a6ac4",
+    "url": "/weather/static/js/runtime-main.f3e6d84a.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/weather/static/media/image.cc54c19b.jpg"
+  }
+]);
